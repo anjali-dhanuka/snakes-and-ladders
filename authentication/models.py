3ff4d3efbe406781feed05eb3bff93cef2578638
anjali <anjalidhanuka99@gmail.com>
2017-07-23 20:26:47 +0530

    models created

--- authentication/models.py
+++ authentication/models.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
+class profile(models.Model):
+   user = models.OneToOneField(User)
+   score = models.IntegerField()
 
 
 
