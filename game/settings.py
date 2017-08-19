910ac60db68291cb4aa30ee79b81bc9eefd53eba
anjali <anjalidhanuka99@gmail.com>
2017-08-19 12:57:45 +0530

    additional files and changes for Heroku

--- game/settings.py
+++ game/settings.py
@@ -142,3 +142,16 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
